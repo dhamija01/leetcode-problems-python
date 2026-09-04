cc74dd8781283d35127413c034282671b605a9e9
Vishu Dhamija <86561491+dhamija01@users.noreply.github.com>
2026-09-04 17:58:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (71.43%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.py
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.py
@@ -1,12 +1,21 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
         l = len(nums)
-        for i in range(l):
-            maxi= max(nums[0:i+1])
-            mini = min(nums[i:l])
+        
+        prefix_max= [0]*l
+        prefix_max[0]= nums[0]
+        for i in range(1,l):
+            prefix_max[i]= max(prefix_max[i-1],nums[i])
 
-            if maxi-mini <=k:
+
+        suffix_min = [0]*l
+        suffix_min[l-1]=nums[l-1]
+        for i in range(l-2,-1,-1):
+            suffix_min[i] = min(suffix_min[i+1],nums[i])
+
+        
+        for i in range(l):
+            if prefix_max[i] - suffix_min[i] <= k:
                 return i
 
         return -1
-            
